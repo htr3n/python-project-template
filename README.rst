8f9aac57ef8d62e350e978af3380fcff90437dbd
Huy Tran <hoang.huy.tran@gmail.com>
2018-01-07 07:31:39 +1100

    First commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+# Python Project Template
+
+This project contains a simple skeleton for a typical Python project, derived from Kenneth Reitz's <https://github.com/kennethreitz/samplemod>.
+
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='python-project-template',
+    version='0.0.1',
+    description='Python project template',
+    long_description=readme,
+    author='Huy Tran',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/htr3n/python-project-template',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
